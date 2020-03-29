b24b1a35d6554bd1a3136021db96e97c2660f70c
魔都一只土拨鼠 <imgoogege@gmail.com>
2020-03-29 22:47:13 +0800

    add

--- stack_test.go
+++ stack_test.go
@@ -6,20 +6,20 @@ import (
 )
 
 func TestStack(t *testing.T) {
-	stack := NewStack()
+	stack := NewStack(3)
 	fmt.Println(stack.Length()) // 0
 	fmt.Println(stack.Push(1))  //1
 	fmt.Println(stack.Pop())    //1
-	fmt.Println(stack.Pop())   // nil
-	fmt.Println(stack.Top())   // nil
-	fmt.Println(stack.Push(1)) // 1
-	fmt.Println(stack.Push(2)) // 2
-	fmt.Println(stack.Push(3)) // 3
+	fmt.Println(stack.Pop())    // nil
+	fmt.Println(stack.Top())    // nil
+	fmt.Println(stack.Push(1))  // 1
+	fmt.Println(stack.Push(2))  // 2
+	fmt.Println(stack.Push(3))  // 3
 	fmt.Println(stack.Length()) // 3
-	fmt.Println(stack.Top()) // 3
-	fmt.Println(stack.Pop()) // 3
-	fmt.Println(stack.Pop()) // 2
-	fmt.Println(stack.Pop()) // 1
-	fmt.Println(stack.Pop()) // nil
+	fmt.Println(stack.Top())    // 3
+	fmt.Println(stack.Pop())    // 3
+	fmt.Println(stack.Pop())    // 2
+	fmt.Println(stack.Pop())    // 1
+	fmt.Println(stack.Pop())    // nil
 	fmt.Println(stack.Length()) // 0
 }
