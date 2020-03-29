73e5f0f55c1fbb00f1a7f9c530643afc10b006f6
魔都一只土拨鼠 <imgoogege@gmail.com>
2020-03-29 21:46:15 +0800

    add

--- stack_test.go
+++ stack_test.go
@@ -15,9 +15,11 @@ func TestStack(t *testing.T) {
 	fmt.Println(stack.Push(1)) // 1
 	fmt.Println(stack.Push(2)) // 2
 	fmt.Println(stack.Push(3)) // 3
+	fmt.Println(stack.Length()) // 3
 	fmt.Println(stack.Top()) // 3
 	fmt.Println(stack.Pop()) // 3
 	fmt.Println(stack.Pop()) // 2
 	fmt.Println(stack.Pop()) // 1
 	fmt.Println(stack.Pop()) // nil
+	fmt.Println(stack.Length()) // 0
 }
